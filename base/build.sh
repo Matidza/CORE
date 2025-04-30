8fb20de248294767037d4239873bdd964317a5a8
Matidza M.Z <125007667+Matidza@users.noreply.github.com>
2025-04-30 13:08:22 +0200

    Delete base/build.sh

--- base/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Create and activate virtual environment
-python -m venv /opt/venv
-source /opt/venv/bin/activate
-
-# Upgrade pip and install dependencies
-pip install --upgrade pip
-pip install -r requirements.txt
