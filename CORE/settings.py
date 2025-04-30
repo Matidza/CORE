6d354072fc09b413827c91bd7fe61a2184f400fe
Matidza M.Z <125007667+Matidza@users.noreply.github.com>
2025-04-30 13:12:07 +0200

    Update settings.py

--- CORE/settings.py
+++ CORE/settings.py
@@ -21,12 +21,14 @@ DEBUG = True
 
 """ """
 ALLOWED_HOSTS = [ 
+    'admission-school-details.up.railway.app',
     'localhost',
     '127.0.0.1',
  
 ]
 
 CSRF_TRUSTED_ORIGINS = [
+    'https://admission-school-details.up.railway.app',
     'http://localhost',
     'http://127.0.0.1',
 ]
