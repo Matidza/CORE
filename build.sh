b929cf953f137608e7f1df2c8095eaea708de70b
Matidza M.Z <125007667+Matidza@users.noreply.github.com>
2025-05-01 04:45:10 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,1 @@
-#!/bin/bash
-
-# Create and activate virtual environment
-python -m venv /opt/venv
-source /opt/venv/bin/activate
-
-# Upgrade pip and install dependencies
-pip install --upgrade pip
-pip install -r requirements.txt
+#!/bin/bash python manage.py migrate python manage.py collectstatic --noinput 
